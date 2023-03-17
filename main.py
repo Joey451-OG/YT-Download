50f4e86a9465f596ae93509b02a0b9a41cd25c16
ItIsJoeyG <itsjoeygofficial@outlook.com>
2023-03-16 19:46:44 -0500

    Fixed Formatting error

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # LICENSE
 '''
 YT Download. A simple GUI wrapper for yt-dlp.
-    Copyright (C) 2023 Joey451-OG
+    Copyright (C) 2023  Joey451-OG
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
