fed152049d9ad848627637a4d4778d2945ac8a20
ItIsJoeyG <itsjoeygofficial@outlook.com>
2023-03-24 10:07:36 -0500

    removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def test (name:str, age:int, isSleeping: bool):
-    if isSleeping:
-        sleep = 'is sleeping'
-    else:
-        sleep = 'is not sleeping'
-    print(f'{name} (age: {age}), {sleep}.')
-
-test(18, 'Joey', False)
